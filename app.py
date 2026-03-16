b2c58376a93666e57d8040c5c1b7603e1a72c0bb
Sudipta Hazra <63796688+Somu796@users.noreply.github.com>
2026-03-16 16:26:46 +0000

    commented out plotting in app.py

--- app.py
+++ app.py
@@ -51,34 +51,34 @@ def main() -> None:
         )
     )
 
-    cart.plot_3d(
-        variable="burst_count",
-        path=f"{ASSETS_DIR}/01_cartesian_grid_with_burst_counts.json",
-        show_earth=True,
-        earth_image_path_str=f"{project_root}/assets/temp.jpg",
-        show_sun=False,
-    )
-    cart.plot_3d(
-        variable="observation_time",
-        path=f"{ASSETS_DIR}/02_cartesian_grid_with_observation_time.json",
-        show_earth=True,
-        earth_image_path_str=f"{project_root}/assets/temp.jpg",
-        show_sun=False,
-    )
-    cart.plot_3d(
-        variable="residence_time",
-        path=f"{ASSETS_DIR}/03_cartesian_grid_with_residence_time.json",
-        show_earth=True,
-        earth_image_path_str=f"{project_root}/assets/temp.jpg",
-        show_sun=False,
-    )
-    cart.plot_3d(
-        variable="normalised_observation_time",
-        path=f"{ASSETS_DIR}/04_cartesian_grid_with_normalised_observation_time.json",
-        show_earth=True,
-        earth_image_path_str=f"{project_root}/assets/temp.jpg",
-        show_sun=False,
-    )
+    # cart.plot_3d(
+    #     variable="burst_count",
+    #     path=f"{ASSETS_DIR}/01_cartesian_grid_with_burst_counts.json",
+    #     show_earth=True,
+    #     earth_image_path_str=f"{project_root}/assets/temp.jpg",
+    #     show_sun=False,
+    # )
+    # cart.plot_3d(
+    #     variable="observation_time",
+    #     path=f"{ASSETS_DIR}/02_cartesian_grid_with_observation_time.json",
+    #     show_earth=True,
+    #     earth_image_path_str=f"{project_root}/assets/temp.jpg",
+    #     show_sun=False,
+    # )
+    # cart.plot_3d(
+    #     variable="residence_time",
+    #     path=f"{ASSETS_DIR}/03_cartesian_grid_with_residence_time.json",
+    #     show_earth=True,
+    #     earth_image_path_str=f"{project_root}/assets/temp.jpg",
+    #     show_sun=False,
+    # )
+    # cart.plot_3d(
+    #     variable="normalised_observation_time",
+    #     path=f"{ASSETS_DIR}/04_cartesian_grid_with_normalised_observation_time.json",
+    #     show_earth=True,
+    #     earth_image_path_str=f"{project_root}/assets/temp.jpg",
+    #     show_sun=False,
+    # )
 
     ltrmat = (
         # Provide bin size for the 3D plot
@@ -106,34 +106,34 @@ def main() -> None:
         )
     )
 
-    ltrmat.plot_3d(
-        variable="burst_count",
-        path=f"{ASSETS_DIR}/01_ltrmat_grid_with_burst_counts.json",
-        show_earth=True,
-        earth_image_path_str=f"{project_root}/assets/temp.jpg",
-        show_sun=False,
-    )
-    ltrmat.plot_3d(
-        variable="observation_time",
-        path=f"{ASSETS_DIR}/02_ltrmat_grid_with_observation_time.json",
-        show_earth=True,
-        earth_image_path_str=f"{project_root}/assets/temp.jpg",
-        show_sun=False,
-    )
-    ltrmat.plot_3d(
-        variable="residence_time",
-        path=f"{ASSETS_DIR}/03_ltrmat_grid_with_residence_time.json",
-        show_earth=True,
-        earth_image_path_str=f"{project_root}/assets/temp.jpg",
-        show_sun=False,
-    )
-    ltrmat.plot_3d(
-        variable="normalised_observation_time",
-        path=f"{ASSETS_DIR}/04_ltrmat_grid_with_normalised_observation_time.json",
-        show_earth=True,
-        earth_image_path_str=f"{project_root}/assets/temp.jpg",
-        show_sun=False,
-    )
+    # ltrmat.plot_3d(
+    #     variable="burst_count",
+    #     path=f"{ASSETS_DIR}/01_ltrmat_grid_with_burst_counts.json",
+    #     show_earth=True,
+    #     earth_image_path_str=f"{project_root}/assets/temp.jpg",
+    #     show_sun=False,
+    # )
+    # ltrmat.plot_3d(
+    #     variable="observation_time",
+    #     path=f"{ASSETS_DIR}/02_ltrmat_grid_with_observation_time.json",
+    #     show_earth=True,
+    #     earth_image_path_str=f"{project_root}/assets/temp.jpg",
+    #     show_sun=False,
+    # )
+    # ltrmat.plot_3d(
+    #     variable="residence_time",
+    #     path=f"{ASSETS_DIR}/03_ltrmat_grid_with_residence_time.json",
+    #     show_earth=True,
+    #     earth_image_path_str=f"{project_root}/assets/temp.jpg",
+    #     show_sun=False,
+    # )
+    # ltrmat.plot_3d(
+    #     variable="normalised_observation_time",
+    #     path=f"{ASSETS_DIR}/04_ltrmat_grid_with_normalised_observation_time.json",
+    #     show_earth=True,
+    #     earth_image_path_str=f"{project_root}/assets/temp.jpg",
+    #     show_sun=False,
+    # )
 
     # Saving the Data
     cart.save_grid(path=f"{ASSETS_DIR}/cart_akr_grid.parquet", fmt="parquet")
