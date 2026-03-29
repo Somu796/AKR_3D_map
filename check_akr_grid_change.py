9db33beb5c190ae9694140c4ffeb066857e95cb2
astro visitor <visitor_ap8@fnode.ap.dias.ie>
2026-03-29 20:55:55 +0100

    Observation time> residence time problem solved and was due to missing period even in high res data.

--- check_akr_grid_change.py
+++ check_akr_grid_change.py
@@ -9,7 +9,7 @@ def main() -> None:
     sys.path.append(str(project_root))
 
     # Define standard data subdirectories
-    ASSETS_DIR = project_root / "assets" / "3D_Objects_02"
+    ASSETS_DIR = project_root / "assets" / "3D_Objects_03"
 
     # Load data
     df = pd.read_parquet(path=f"{ASSETS_DIR}/cart_akr_grid.parquet")
